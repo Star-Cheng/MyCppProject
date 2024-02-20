aaae21d32e038ebbb8a052f5d696947f8a8c3a44
Star-Cheng <1335929010@qq.com>
2024-02-20 16:51:42 +0800

    Feb 20th, 2024 1st

---
+++ Log/Study/04DataType.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"hello world" "\n";
+    // // 1. 短整型（-32768--32767）
+    // short num1 = 32767;
+    // cout<<num1 <<"\n";
+    return 0;
+}
