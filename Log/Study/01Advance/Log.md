73b0b08352c2b13293180cca164874320e7e5d6a
Gym <1335929010@qq.com>
2024-05-24 13:21:34 +0800

    May 24th. 2024 2nd

--- Log/Study/01Advance/Log.md
+++ Log/Study/01Advance/Log.md
@@ -2117,3 +2117,65 @@ main()
 ### 4.7 多态
 
 #### 4.7.1 多态的基本概念
+
++ 多态分为两类
+    1. 静态多态
+    2. 动态多态
+
++ 静态多态和动态多态的区别
+    1. 静态多态的函数地址早绑定 - 编译阶段确定函数地址
+    2. 动态多态的函数地址晚绑定 - 运行阶段确定函数地址
+
+```C++
+#include <iostream>
+using namespace std;
+
+// 多态
+class Animal
+{
+public:
+    // void speak()
+    virtual void speak()
+    {
+        cout << "Animal speak" << endl;
+    }
+};
+class Cat : public Animal
+{
+public:
+    void speak()
+    {
+        cout << "Cat speak" << endl;
+    }
+};
+class Dog : public Animal
+{
+public:
+    void speak()
+    {
+        cout << "Dog speak" << endl;
+    }
+};
+// 地址早绑定, 再编译阶段就确定了函数地址
+// 如果想执行让猫说话, 那么这个函数地址就不能提前绑定, 需要在运行阶段进行绑定
+// 动态多态满足条件
+// 1. 有继承关系
+// 2. 子类要重写父类的虚函数
+// 动态多态使用: 父类的指针或者引用 指向子类对象
+void doSpeak(Animal &a)
+{
+    a.speak();
+}
+void test01()
+{
+    Cat c;
+    doSpeak(c);
+    Dog d;
+    doSpeak(d);
+}
+main()
+{
+    test01();
+    return 0;
+}
+```
--- Log/Study/01Advance/Temp.cpp
+++ Log/Study/01Advance/Temp.cpp
@@ -1,37 +1,48 @@
 #include <iostream>
 using namespace std;
 
-// 动物类
+// 多态
 class Animal
 {
 public:
-    int m_Age;
+    // void speak()
+    virtual void speak()
+    {
+        cout << "Animal speak" << endl;
+    }
 };
-// 利用虚继承解决菱形继承问题
-// 继承之前加上关键字virtual
-// Animal类称为虚基类
-// 羊类
-class Sheep : virtual public Animal
+class Cat : public Animal
 {
+public:
+    void speak()
+    {
+        cout << "Cat speak" << endl;
+    }
 };
-// 驼类
-class Horse : virtual public Animal
+class Dog : public Animal
 {
+public:
+    void speak()
+    {
+        cout << "Dog speak" << endl;
+    }
 };
-// 羊驼类
-class SheepHorse : public Sheep, public Horse
+// 地址早绑定, 再编译阶段就确定了函数地址
+// 如果想执行让猫说话, 那么这个函数地址就不能提前绑定, 需要在运行阶段进行绑定
+// 动态多态满足条件
+// 1. 有继承关系
+// 2. 子类要重写父类的虚函数
+// 动态多态使用: 父类的指针或者引用 指向子类对象
+void doSpeak(Animal &a)
 {
-};
+    a.speak();
+}
 void test01()
 {
-    SheepHorse s;
-    s.Sheep::m_Age = 20;
-    s.Horse::m_Age = 30;
-    // 当菱形继承, 当两个父类拥有相同数据, 需要加以作用域区分
-    cout << "s.Sheep::m_Age = " << s.Sheep::m_Age << endl;
-    cout << "s.Horse::m_Age = " << s.Horse::m_Age << endl;
-    cout << "s.m_Age = " << s.m_Age << endl;
-    // m_Age我们只需要一份就可以了, 菱形继承导致数据有两份, 资源浪费
+    Cat c;
+    doSpeak(c);
+    Dog d;
+    doSpeak(d);
 }
 main()
 {
